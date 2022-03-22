88bd935345145d602c368157f0d91e1bdbfd8a68
Christopher Hengler <dr.c.hengler@gmail.com>
2022-03-22 17:51:58 +0100

    move token tests to pytest

--- test/test_rant_token.py
+++ test/test_rant_token.py
@@ -1,20 +1,17 @@
-import unittest
-
 from rant_token import RantToken, RantTokenType
 
-class RantTokenTest(unittest.TestCase):
-    def testRantTokenEquality(self):
-        plaintext = RantToken(RantTokenType.PLAIN_TEXT,"plaintext")
-        duplicate_plaintext = RantToken(RantTokenType.PLAIN_TEXT,"plaintext")
-        different_plaintext = RantToken(RantTokenType.PLAIN_TEXT,"different plaintext")
-        empty_plaintext = RantToken(RantTokenType.PLAIN_TEXT)
-        left_angle = RantToken(RantTokenType.LEFT_ANGLE_BRACKET)
-        hyphen = RantToken(RantTokenType.HYPHEN)
+def test_rant_token_equality():
+    plaintext = RantToken(RantTokenType.PLAIN_TEXT,"plaintext")
+    duplicate_plaintext = RantToken(RantTokenType.PLAIN_TEXT,"plaintext")
+    different_plaintext = RantToken(RantTokenType.PLAIN_TEXT,"different plaintext")
+    empty_plaintext = RantToken(RantTokenType.PLAIN_TEXT)
+    left_angle = RantToken(RantTokenType.LEFT_ANGLE_BRACKET)
+    hyphen = RantToken(RantTokenType.HYPHEN)
 
 
-        self.assertEqual(plaintext,duplicate_plaintext)
-        self.assertNotEqual(plaintext,empty_plaintext)
-        self.assertNotEqual(plaintext,left_angle)
-        self.assertNotEqual(plaintext,different_plaintext)
-        self.assertNotEqual(left_angle,hyphen)
+    assert plaintext == duplicate_plaintext
+    assert plaintext != empty_plaintext
+    assert plaintext != left_angle
+    assert plaintext != different_plaintext
+    assert left_angle != hyphen
 
