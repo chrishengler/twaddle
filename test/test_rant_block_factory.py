14fe03caba649daa331bd33ac7d77fea7beb878c
Christopher Hengler <dr.c.hengler@gmail.com>
2022-04-06 16:50:33 +0200

    some more block factory tests

--- test/test_rant_block_factory.py
+++ test/test_rant_block_factory.py
@@ -14,3 +14,25 @@ def test_parse_choice():
         assert isinstance(choice[0], RantTextObject)
     assert choice_result.choices[0][0].text == "this"
     assert choice_result.choices[1][0].text == "that"
+
+
+def test_parse_block_with_embedded_lookup():
+    lex_result = RantLexer.lex("{<lookup>}")
+    result = BlockFactory.build(lex_result)
+    assert result.type == RantObjectType.BLOCK
+    assert len(result.choices) == 1
+    assert len(result.choices[0]) == 1
+    assert isinstance(result.choices[0][0], RantLookupObject)
+    assert result.choices[0][0].dictionary == "lookup"
+
+
+def test_parse_choice_with_embedded_lookups():
+    lex_result = RantLexer.lex("{<lookup1>|<lookup2>|<lookup3>}")
+    result = BlockFactory.build(lex_result)
+    assert result.type == RantObjectType.BLOCK
+    assert len(result.choices) == 3
+    for i, choice in enumerate(result.choices, 1):
+        assert len(choice) == 1
+        assert isinstance(choice[0], RantLookupObject)
+    assert choice[0].dictionary == ("lookup" + str(i))
+
