a261da5f55e0e2358c3ede63b30dd8bb8efb885e
Christopher Hengler <dr.c.hengler@gmail.com>
2022-05-10 17:28:02 +0200

    lookups with tags (positive and negative)

--- pyrant/lookup/lookup.py
+++ pyrant/lookup/lookup.py
@@ -3,11 +3,17 @@ from random import choice
 from rant_exceptions import RantLookupException
 
 class LookupEntry:
-    def __init__(self, forms: OrderedDict[str,str]):
+    def __init__(self, forms: OrderedDict[str,str], tags: set[str] = None):
         self.forms = forms
+        self.tags = tags
 
     def __getitem__(self, form: str):
         return self.forms[form]
+
+    def has_any_tag_of(self, tags: set[str]) -> bool:
+        if tags is None or self.tags is None:
+            return False
+        return not self.tags.isdisjoint(tags)
 
 class LookupDictionary:
     def __init__(self, name: str, forms: list[str]):
@@ -15,10 +21,28 @@ class LookupDictionary:
         self.forms = forms
         self.entries = list[LookupEntry]()
 
-    def add(self, entry: LookupEntry):
-        self.entries.append(entry)
+    def add(self, forms: list[str], tags: set[str] = None):
+        if len(forms) != len(self.forms):
+            raise RantLookupException("[LookupDictionary.add] wrong number of forms provided")
+        lookup = LookupEntry(OrderedDict(zip(self.forms, forms)), tags)
+        self.entries.append(lookup)
 
-    def get(self, form: str):
+    def get(self, form: str, tags_positive: set[str] = None, tags_negative: set[str] = None) -> str:
         if form not in self.forms:
             raise RantLookupException(f"[LookupDictionary.get] dictionary '{self.name}' has no form '{form}'")
-        return choice(self.entries)[form]
+        valid_choices = list[LookupEntry]()
+        if not tags_negative  and not tags_positive:
+            valid_choices = self.entries
+        else:
+            for entry in self.entries:
+                valid = False
+                if not tags_positive or entry.has_any_tag_of(tags_positive):
+                    valid = True
+                if entry.has_any_tag_of(tags_negative):
+                    valid = False
+                if valid:
+                    valid_choices.append(entry)
+        if not valid_choices:
+            valid_choices = self.entries
+        return choice(valid_choices)[form]
+
--- test/lookup/test_lookup.py
+++ test/lookup/test_lookup.py
@@ -2,23 +2,36 @@ from rant_exceptions import RantLookupException
 from lookup.lookup import *
 import pytest
 
-lookup = LookupEntry({"singular": "thing",
-                      "plural": "things",
-                      "possessive": "thing's",
-                      "pluralpossessive": "things'"})
-
 def test_lookup_type():
-    assert lookup["singular"] == "thing"
-    assert lookup["plural"] == "things"
-    assert lookup["possessive"] == "thing's"
-    assert lookup["pluralpossessive"] == "things'"
+    lookup_thing = LookupEntry({"singular": "thing",
+                                "plural": "things",
+                                "possessive": "thing's",
+                                "pluralpossessive": "things'"})
+    assert lookup_thing["singular"] == "thing"
+    assert lookup_thing["plural"] == "things"
+    assert lookup_thing["possessive"] == "thing's"
+    assert lookup_thing["pluralpossessive"] == "things'"
 
 def test_dictionary():
-    dictionary = LookupDictionary("noun", ["singular", "plural", "possessive", "pluralpossessive"])
-    dictionary.add(lookup)
+    dictionary = LookupDictionary("noun", ["singular", "plural"])
+    dictionary.add(["hexagon", "hexagons"])
     assert len(dictionary.entries) == 1
-    assert dictionary.get("singular") == "thing"
-    assert dictionary.get("pluralpossessive") == "things'"
+    assert dictionary.get("singular") == "hexagon"
+    assert dictionary.get("plural") == "hexagons"
     with pytest.raises(RantLookupException) as e_info:
         dictionary.get("invalid")
         assert e_info.message == "[LookupDictionary.get] dictionary 'noun' has no form 'invalid'"
+
+def test_tag_requirement():
+    dictionary = LookupDictionary("noun", ["singular", "plural"])
+    dictionary.add(["thing", "things"], set(["tag1"]))
+    dictionary.add(["hexagon", "hexagons"], set(["tag2"]))
+    for _ in range(0,5):
+        assert dictionary.get("plural", {"tag1"}) == "things"
+        assert dictionary.get("singular", {"tag2"}) == "hexagon"
+
+lookup = LookupEntry({"s": "s",
+                      "pl": "pl"}, {"tag1"})
+
+if __name__ == "__main__":
+    test_tag_requirement()
