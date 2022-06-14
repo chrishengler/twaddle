b3f160331359446d76294869e2e4ce3f9d35362f
Chris Hengler <dr.c.hengler@gmail.com>
2022-06-14 16:23:06 +0200

    refactor lookup._get() to reduce complexity

--- pyrant/lookup/lookup.py
+++ pyrant/lookup/lookup.py
@@ -43,6 +43,41 @@ class LookupDictionary:
     def clear_labels(self):
         self.labels = dict[str, LookupEntry]()
 
+    def _get_form(self, form: str) -> str:
+        if form is None:
+            form = self.forms[0]
+        if form not in self.forms:
+            raise RantLookupException(
+                f"[LookupDictionary.get] dictionary '{self.name}' has no form '{form}'"
+            )
+        return form
+
+    def _get_valid_choices(
+        self,
+        tags_positive: set[str] = None,
+        tags_negative: set[str] = None,
+        labels_negative: set[str] = None,
+    ) -> list[LookupEntry]:
+        valid_choices = list[LookupEntry]()
+        for entry in self.entries:
+            if entry.has_any_tag_of(tags_negative):
+                continue
+            if not tags_positive or entry.has_all_tags(tags_positive):
+                valid_choices.append(entry)
+        if not valid_choices:
+            valid_choices = list.copy(self.entries)
+        valid_choices = self._prune_valid_choices(valid_choices, labels_negative)
+        return valid_choices
+
+    def _prune_valid_choices(
+        self, valid_choices: list[LookupEntry], labels_negative: set[str]
+    ) -> list[LookupEntry]:
+        if labels_negative:
+            for label in labels_negative:
+                if label in self.labels and self.labels[label] in valid_choices:
+                    valid_choices.remove(self.labels[label])
+        return valid_choices
+
     def _get(
         self,
         form: str = None,
@@ -51,32 +86,14 @@ class LookupDictionary:
         label_positive: str = None,
         labels_negative: set[str] = None,
     ) -> str:
-        if form is None:
-            form = self.forms[0]
-        if form not in self.forms:
-            raise RantLookupException(
-                f"[LookupDictionary.get] dictionary '{self.name}' has no form '{form}'"
-            )
+        form = self._get_form(form)
         if label_positive and label_positive in self.labels:
             return self.labels[label_positive][form]
-        valid_choices = list[LookupEntry]()
-        if not tags_negative and not tags_positive:
-            valid_choices = list.copy(self.entries)
-        else:
-            for entry in self.entries:
-                valid = False
-                if not tags_positive or entry.has_all_tags(tags_positive):
-                    valid = True
-                if entry.has_any_tag_of(tags_negative):
-                    valid = False
-                if valid:
-                    valid_choices.append(entry)
-        if not valid_choices:
-            valid_choices = self.entries
-        if labels_negative:
-            for label in labels_negative:
-                if label in self.labels and self.labels[label] in valid_choices:
-                    valid_choices.remove(self.labels[label])
+
+        valid_choices = self._get_valid_choices(
+            tags_positive, tags_negative, labels_negative
+        )
+
         chosen_entry = choice(valid_choices)
         if label_positive:
             self.labels[label_positive] = chosen_entry
